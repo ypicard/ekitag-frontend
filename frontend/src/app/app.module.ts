50754c39013519adf446c3c637c5e84658e46939
ypicard <yann.picard95@gmail.com>
2017-11-24 14:32:09 +0100

    Add mother fucking bootstrap 4 (& ng-bootstrap for angular directives)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,7 +27,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-     RouterModule.forRoot(
+    RouterModule.forRoot(
        appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
