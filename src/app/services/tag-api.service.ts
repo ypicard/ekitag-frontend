d05d001ecde63ba29d5d4386f8779ad622e756f2
ypicard <yann.picard95@gmail.com>
2018-02-27 23:19:06 +0100

    Fix season duration

--- src/app/services/tag-api.service.ts
+++ src/app/services/tag-api.service.ts
@@ -179,10 +179,10 @@ export class TagApiService {
     });
   }
 
-  createSeason(name: string, maxMatches: number, maxDays: number): Observable<any> {
+  createSeason(name: string, maxMatches: number, maxTimeInSeconds: number): Observable<any> {
     let formData = new FormData();
     formData.append('name', name);
-    formData.append('max_time', maxDays + ' days'); // Postgres interval
+    formData.append("max_time", maxTimeInSeconds.toString());
     formData.append('max_matches', maxMatches.toString());
 
     return this.http.post(this.BASE_URL + 'seasons', formData, {
