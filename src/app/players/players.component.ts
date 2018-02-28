f9672697c4277e1a33550d03243e66fc803cc59d
Yann Picard <yann.picard95@gmail.com>
2018-02-28 13:24:20 +0100

    Fix players template

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,69 +1,54 @@
-import {
-  Component
-} from '@angular/core';
-import {
-  TagApiService
-} from '../services/tag-api.service';
-import {
-  FormsModule
-} from '@angular/forms';
-import {
-  ActivatedRoute
-} from '@angular/router';
+import { Component } from '@angular/core';
+import { TagApiService } from '../services/tag-api.service';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Player } from '../_models/player.model';
 
 @Component({
   selector: 'players',
   templateUrl: './players.template.html',
   styleUrls: ['./players.style.scss']
 })
-
 export class PlayersComponent {
+  players: Player[];
 
-  players: any;
-  pseudo: string;
-  trigram: string;
-
-  constructor(public route: ActivatedRoute, public tagApiService: TagApiService) {
+  constructor(
+    public route: ActivatedRoute,
+    public tagApiService: TagApiService
+  ) {
     console.log('PlayersComponent');
     this.route.data.subscribe(val => {
       this.players = val.players;
     });
   }
 
-  createUser(trigram, pseudo) {
-    if (!trigram || !pseudo) {
-      alert('A player must have a trigram and a pseudo.')
-      return;
-    }
-
-    trigram = trigram.toLowerCase(); // Only send downcase
-
-    this.tagApiService.createUser(trigram, pseudo).subscribe(res => {
-      alert(res['message'])
-      this.refreshUI()
-    }, error => {
-      alert(error.error.message)
-    })
+  createUser(form) {
+    this.tagApiService.createUser(form.value.trigram, form.value.pseudo).subscribe(
+      res => {
+        alert(res['message']);
+        this.refreshUI();
+      },
+      error => {
+        alert(error.error.message);
+      }
+    );
   }
 
   refreshUI() {
-    this.trigram = null;
-    this.pseudo = null;
     this.tagApiService.getAllUsers().subscribe(res => {
       this.players = res;
-    })
+    });
   }
 
   getActivePlayers() {
     return this.players.filter(player => {
-      return player.is_active;
-    })
+      return player.isActive;
+    });
   }
 
-  getUnactivePlayers() {
+  getInactivePlayers() {
     return this.players.filter(player => {
-      return !player.is_active;
-    })
+      return !player.isActive;
+    });
   }
-
 }
