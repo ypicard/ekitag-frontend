7247e2029118022aa4e5a1994ff4206c31617e5f
Yann Picard <yann.picard95@gmail.com>
2018-04-05 13:11:47 +0200

    Add rank to user custom stats

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -77,6 +77,7 @@ export class PlayerComponent {
     // User custom stats
     this.tagApiService.getUserCustomStats(this.player.id).subscribe(res => {
       this.userCustomStats = res;
+      console.log(res)
 
       // Ratings data
       this.charts.ratings.data.push(Object.entries(res).reduce((obj, el) => {
